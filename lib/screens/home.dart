afd3b162b73da9450092e606c85c912d99782c3e
Nirad <yeola.nirad@gmail.com>
2021-05-05 01:15:03 +0530

    UI changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/techicon_icons.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -7,9 +8,13 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         //backgroundColor: Gradient(),
         toolbarHeight: 93.0,
-        leading:CircleAvatar(
-          radius: 80.0,
-          backgroundColor: Colors.blue,
+        
+        leading:Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: CircleAvatar(
+            radius: 50.0,
+            backgroundColor: Colors.blue,
+          ),
         ),
         title: Text('Name'),
         actions: [
@@ -42,17 +47,54 @@ class Home extends StatelessWidget {
 
         ],
       ),
-      body: Center(child: Text('Sample'),),
+      body: Column(
+        
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.all(20),
+            child: Text('NEWS'),
+            ),
+          Container(
+            height: 404,
+            width: 259,
+            decoration: BoxDecoration(
+              
+              border: Border.all(width: 2.0, color: Color(0xFFEACC00),),
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: AssetImage('images/Image_21.png'),
+                fit: BoxFit.fill,
+                alignment: Alignment.topCenter),
+              ),
+            child: Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.fromLTRB(0, 350, 0, 0),
+              decoration: BoxDecoration(
+                color: Color(0xFF171732),
+                borderRadius: BorderRadius.vertical(bottom: Radius.circular(12),top: Radius.circular(0)),
+                ),
+              child: Text(
+                'Nike Joyride',),
+            ),
+            ), 
+          ],
+          ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
-            label: 'Rank',
-            icon: Icon(Icons.emoji_events_outlined),
+            icon: Icon(Techicon.technology_1, size: 35,),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.emoji_events_outlined, size: 35,),
             ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_outlined),
-            label: 'Cart',
+            icon: Icon(Icons.shopping_cart_outlined, size: 35,),
+            label: '',
           ),
+          
         
       ],),
     );
